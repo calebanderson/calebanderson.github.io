49cf581f5ddc5cc571021e0835edf03155ceaef4
calebanderson <caleb.r.anderson.1@gmail.com>
2022-09-25 21:07:04 -0400

    Attempting to ensure the youtube shim is removed before importing

--- youtube-playback/core.js
+++ youtube-playback/core.js
@@ -1,28 +1,31 @@
 // de-shimming a ES5 shim that youtube uses
-customElements.define = CustomElementRegistry.prototype.define;
-customElements.get    = CustomElementRegistry.prototype.get;
-HTMLElement = HTMLElement.prototype.constructor;
+new Promise(function(resolve) {
+  customElements.define = CustomElementRegistry.prototype.define;
+  customElements.get = CustomElementRegistry.prototype.get;
+  HTMLElement = HTMLElement.prototype.constructor;
+  resolve();
+}).then(function(){
+  import CustomVideo from './custom_video.js';
+  import CustomCookie from './custom_cookie.js';
+  window.customCookie = CustomCookie;
 
-import CustomVideo from './custom_video.js';
-import CustomCookie from './custom_cookie.js';
-window.customCookie = CustomCookie;
-
-// Wanted to try out promises... obviously not the most useful implementation...
-function repeatUntilPresent(func, delay){
-  const checkForPresence = function(resolve){
-    const result = func();
-    if(result){
-      resolve(result);
-    } else {
-      setTimeout(checkForPresence.bind(this, resolve), delay);
+  // Wanted to try out promises... obviously not the most useful implementation...
+  function repeatUntilPresent(func, delay){
+    const checkForPresence = function(resolve){
+      const result = func();
+      if(result){
+        resolve(result);
+      } else {
+        setTimeout(checkForPresence.bind(this, resolve), delay);
+      }
     }
+    return new Promise(checkForPresence);
   }
-  return new Promise(checkForPresence);
-}
 
-Promise.all([
-  repeatUntilPresent(()=>document.querySelector(CustomVideo.videoElementSelector), 250),
-  repeatUntilPresent(()=>document.querySelector(CustomVideo.badgeContainerSelector), 250)
-]).then(([video, container])=>{
-  window.customVideo = new CustomVideo(video);
+  Promise.all([
+    repeatUntilPresent(()=>document.querySelector(CustomVideo.videoElementSelector), 250),
+    repeatUntilPresent(()=>document.querySelector(CustomVideo.badgeContainerSelector), 250)
+  ]).then(([video, container])=>{
+    window.customVideo = new CustomVideo(video);
+  });
 });
