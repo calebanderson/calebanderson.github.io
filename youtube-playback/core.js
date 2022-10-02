aea5c25a48ce2c30f4a5c38cf7226eb22a6f4bf3
calebanderson <caleb.r.anderson.1@gmail.com>
2022-10-02 12:05:03 -0400

    Added more console statements to test extension's order of operations

--- youtube-playback/core.js
+++ youtube-playback/core.js
@@ -14,12 +14,13 @@ function repeatUntilPresent(func, delay) {
   return new Promise(checkForPresence);
 }
 
-window.foo = 'thing';
+window.testConstant1 ||= 'core.js testConstant1';
+console.log(`after testConstant1 ||= setter: ${window.testConstant1}`);
 
 Promise.all([
   repeatUntilPresent(() => document.querySelector(CustomVideo.videoElementSelector), 250),
   repeatUntilPresent(() => document.querySelector(CustomVideo.badgeContainerSelector), 250),
 ]).then(([video]) => {
-  console.log(window.foo);
+  console.log(`before CustomVideo creation: ${window.testConstant1}`);
   window.customVideo = new CustomVideo(video);
 });
