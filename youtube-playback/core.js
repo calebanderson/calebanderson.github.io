1ba49e7857dba2288814f3ea3a05457ea6557a7f
calebanderson <caleb.r.anderson.1@gmail.com>
2022-10-02 22:49:32 -0400

    Investigating if DOMContentLoaded combined with document.readyState will be enough Temporary logging again

--- youtube-playback/core.js
+++ youtube-playback/core.js
@@ -2,8 +2,6 @@ import './remove_shim.js';
 import Constants from './constants.js';
 import CustomVideo from './custom_video.js';
 
-window.configScriptSelector ||= 'script[data-source="User JavaScript and CSS extension"]:not([src])';
-
 // Wanted to try out promises... obviously not the most useful implementation...
 function repeatUntilPresent(func, delay) {
   const checkForPresence = (resolve) => {
@@ -17,7 +15,13 @@ function repeatUntilPresent(func, delay) {
   return new Promise(checkForPresence);
 }
 
-repeatUntilPresent(() => document.querySelector(window.configScriptSelector)
-                      && document.querySelector(Constants.badgeContainerSelector)
-                      && document.querySelector(Constants.videoElementSelector), 250)
-  .then((video) => { window.customVideo = new CustomVideo(video); });
+function mountWhenReady() {
+  repeatUntilPresent(() => {
+    console.log('Searching DOM for required elements');
+    return document.querySelector(Constants.badgeContainerSelector)
+        && document.querySelector(Constants.videoElementSelector);
+  }, 250).then((video) => { window.customVideo = new CustomVideo(video); });
+}
+
+if (document.readyState === 'loading') document.addEventListener('DOMContentLoaded', mountWhenReady);
+else mountWhenReady();
