1091be73ca6408f4f017c36780283c758659a37d
calebanderson <caleb.r.anderson.1@gmail.com>
2022-10-02 21:20:18 -0400

    Promising solution for ensuring config customizations are applied before initializing Cleaning up some of the temporary logging

--- youtube-playback/core.js
+++ youtube-playback/core.js
@@ -2,6 +2,8 @@ import './remove_shim.js';
 import Constants from './constants.js';
 import CustomVideo from './custom_video.js';
 
+window.configScriptSelector ||= 'script[data-source="User JavaScript and CSS extension"]:not([src])';
+
 // Wanted to try out promises... obviously not the most useful implementation...
 function repeatUntilPresent(func, delay) {
   const checkForPresence = (resolve) => {
@@ -15,27 +17,7 @@ function repeatUntilPresent(func, delay) {
   return new Promise(checkForPresence);
 }
 
-const scriptSelector = 'script[data-source="User JavaScript and CSS extension"]:not([src])';
-
-repeatUntilPresent(() => {
-  if (!document.querySelector(scriptSelector)) return null;
-  if (!document.querySelector(Constants.badgeContainerSelector)) return null;
-
-  return document.querySelector(Constants.videoElementSelector);
-}, 250).then((video) => {
-  console.log('Promise loaded');
-  window.customVideo = new CustomVideo(video);
-});
-
-Promise.all([
-  repeatUntilPresent(() => {
-    console.log(`Old: ${Constants.videoElementSelector}`);
-    return document.querySelector(Constants.videoElementSelector);
-  }, 250),
-  repeatUntilPresent(() => document.querySelector(Constants.badgeContainerSelector), 250),
-]).then(([video]) => {
-  console.log('Old promise loaded');
-  window.customVideo = new CustomVideo(video);
-});
-
-document.addEventListener('DOMContentLoaded', () => { console.log('DOMContentLoaded'); });
+repeatUntilPresent(() => document.querySelector(window.configScriptSelector)
+                      && document.querySelector(Constants.badgeContainerSelector)
+                      && document.querySelector(Constants.videoElementSelector), 250)
+  .then((video) => { window.customVideo = new CustomVideo(video); });
